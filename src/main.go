fa1f16ab0dcd7ba8ba90ce721483efb4f516d4b1
freleh <frederic.lehmann@scout24.ch>
2021-10-29 19:18:49 +0000

    basic convert to webp

--- src/main.go
+++ src/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"log"
+	"os"
+
+	"github.com/kolesa-team/go-webp/encoder"
+	"github.com/kolesa-team/go-webp/webp"
+
+	_ "image/jpeg"
+	_ "image/png"
+)
 
 func main(){
-	fmt.Println("Hello, World")
+	reader, err := os.Open("images/tangerine-flower.jpg")
+	if err != nil {
+     log.Fatal(err)
+	 }
+	 defer reader.Close()
+
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Create("images/tangerine-flower.webp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	options, err := encoder.NewLossyEncoderOptions(encoder.PresetDefault, 75)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := webp.Encode(f, m, options); err != nil {
+		log.Fatalln(err)
+	}
 }
