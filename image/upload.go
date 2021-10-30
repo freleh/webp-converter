b96deb513d1f58a77e8cbf3ee8fc2eae3ee746d8
freleh <frederic.lehmann@scout24.ch>
2021-10-30 19:36:19 +0000

    wrap up file upload

--- image/upload.go
+++ image/upload.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -15,18 +16,6 @@ func HandleUpload(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	// Check if directory exists, if not create one
-	dir := fmt.Sprintf("%s%s", "./images/", id)
-	_, err := os.Stat(dir)
-	if os.IsNotExist(err) {
-		if err := os.Mkdir(dir, 0777); err != nil {
-			log.Printf("Could not create directory at: HandleUpload")
-			w.WriteHeader(500)
-		}
-	}
-
-	// Save file to directory
-
 	//10 << 20 specifies a maximum upload of 10 MB files.
 	r.ParseMultipartForm(10 << 20)
 
@@ -35,25 +24,70 @@ func HandleUpload(w http.ResponseWriter, r *http.Request) {
 	// the Header and the size of the file
 	file, handler, err := r.FormFile("image")
 	if err != nil {
-		log.Printf("No file with key: image at: HandleUpload")
-		w.WriteHeader(422)
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 	defer file.Close()
 
+	// Check if the file type is supported
+	if !checkIfFileTypeIsSupported(handler.Filename) {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		return
+	}
+
+	// File directory
+	dir := fmt.Sprintf("%s%s", "./images/", id)
+	if err := checkAndCreateDir(dir); err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	// Create target file
-	targetFile, err := os.Create(fmt.Sprintf("%s/%s", dir, handler.Filename))
+	localFilename := fmt.Sprintf("%s/%s", dir, handler.Filename)
+	targetFile, err := os.Create(localFilename)
 	if err != nil {
-		log.Printf("Could not create file at: HandleUpload with: %s/%s because: %s", dir, handler.Filename, err)
-		w.WriteHeader(500)
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	defer targetFile.Close()
 
 	// read all of the contents of our uploaded file into a byte array
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		log.Printf("Could not read bytes at: HandleUpload")
-		w.WriteHeader(500)
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	targetFile.Write(fileBytes)
+}
+
+// Check if the directory dir exists.
+// If not, it gets created.
+func checkAndCreateDir(dir string) error {
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		if err := os.Mkdir(dir, 0777); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Returns true if file type is supported, otherwise returns false
+func checkIfFileTypeIsSupported(filename string) bool {
+	supportedFileTypes := [4]string{"jpeg", "jpg", "png", "webp"}
+
+	filenameParts := strings.Split(filename, ".")
+	fileType := filenameParts[len(filenameParts)-1]
+
+	for _, x := range supportedFileTypes {
+		if x == fileType {
+			return true
+		}
+	}
+
+	return false
 }
--- main.go
+++ main.go
@@ -9,9 +9,13 @@ import (
 	img "./image"
 )
 
-func main() {
+func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/image/upload/{id}", img.HandleUpload).Methods("POST")
-	myRouter.HandleFunc("/image/serve/{id}/{name}.{type}", img.HandleServe).Methods("GET")
+	myRouter.HandleFunc("/image/serve/{id}/{filename}", img.HandleServe).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
+}
+
+func main() {
+	handleRequests()
 }
